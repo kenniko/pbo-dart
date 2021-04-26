ef43a8091f07a5591af96d99c2cd77e1184df1c1
Niko <kenniko@protonmail.com>
2021-04-26 07:57:48 +0700

    Function, Optional Parameter, Lambda Expression

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,29 +1,57 @@
 import 'dart:io';
 
+double luas_segiempat(double panjang, double lebar) {
+  // double hasil;
+  // hasil = panjang * lebar;
+  // return hasil;
+  return panjang * lebar;
+}
+
+void sapa_penonton() {
+  print('Hello penonton!');
+}
+
+String say(String from, String message, {String to, String appName}) {
+  // parameter optional berdasarkan nama parameter
+  return from +
+      ' say ' +
+      message +
+      ((to != null) ? ' to ' + to : '') +
+      ((appName != null) ? ' via ' + appName : '');
+}
+
+String say2(String from, String message,
+    [String to, String appName = 'WhatsApp']) {
+  // parameter optional berdasarkan urutas
+  return from +
+      ' say ' +
+      message +
+      ((to != null) ? ' to ' + to : '') +
+      ((appName != null) ? ' via ' + appName : '');
+}
+
+double luas_segiempat2(double panjang, double lebar) => panjang * lebar;
+
+int doMathOperator(int number1, int number2, Function(int, int) operator) =>
+    operator(number1, number2);
+
 main(List<String> arguments) {
-  int i = 0;
-  while (i < 5) {
-    print('halo ke ' + i.toString());
-    i += 1;
-  }
+  print(say('Johny', 'Hello', appName: 'WhatsApp'));
+  print(say2('Johny', 'Hello', 'Doris'));
 
-  print('======');
+  double p, l, luas;
+  Function f;
+  f = luas_segiempat2;
 
-  for (int counter = 1; counter < 5; counter = counter + 1) {
-    print('halo');
-  }
+  p = double.tryParse(stdin.readLineSync());
+  l = double.tryParse(stdin.readLineSync());
 
-  print('======');
+  luas = luas_segiempat(p, l);
 
-  int x = 0;
-  do {
-    print('dododo ke ' + x.toString());
-    x += 1;
-  } while (x < 5);
+  print(luas);
 
-  int a, b, c;
-  a = 10;
-  b = ++a;
-  c = a++;
-  print(a.toString() + ' ' + b.toString() + ' ' + c.toString());
+  sapa_penonton();
+
+  print(f(6.0, 3.0));
+  print(doMathOperator(2, 3, (a, b) => a * b));
 }
